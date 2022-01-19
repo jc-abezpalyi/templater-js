f02a13bfa83fc377cc9c91b889b6ffb660adc09b
Artur <abezpalyi@justcoded.com>
2022-01-19 16:30:31 +0200

    finish: Templater test task

---
+++ src/templater.js
@@ -0,0 +1,41 @@
+const Templater = {
+  //step 1
+  run: function () {
+    this.tags.forEach(tag => {
+      this.render(tag.template, tag.element);
+    });
+  },
+  //step 2,3
+  tags: [],
+  addTag: function (tag, template) {
+    [...document.querySelectorAll(`${tag}`)].map(el => {
+      let currentTemplate = template;
+      let attrList = [...el.attributes];
+
+      attrList.map(attr => {
+        currentTemplate = currentTemplate.replace(`{{${attr.localName}}}`, attr.value);
+        currentTemplate = currentTemplate.replace(`{{html}}`, `${el.innerHTML === '{{html}}' ? el.innerHTML : 'Some Text'}`);
+      });
+      this.tags.push({
+        element: el,
+        template: currentTemplate,
+      });
+    });
+  },
+  render: (template, element) => {
+    element.outerHTML = template;
+  },
+};
+//   //step 4,5
+
+;(function ($) {
+  $.fn.templater = run;
+  function run({ tags }) {
+    let currentTag = this.children()[0];
+    let text = this.children().text().trim();
+    Object.values(currentTag.attributes).forEach(attr => {
+      tags.button = tags.button.replace(`{{${attr.localName}}}`, attr.value);
+    });
+    this[0].innerHTML = tags.button.replace(/{{(.*?)}}/, text);
+  }
+})(jQuery);
