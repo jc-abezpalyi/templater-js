06e899d5ccf9da746c85dc9115d2901fbca93c50
Artur <abezpalyi@justcoded.com>
2022-01-24 17:11:40 +0200

    rewrite clear js on jq

--- src/templater.js
+++ src/templater.js
@@ -7,23 +7,23 @@ const Templater = {
   },
   //step 2,3
   tags: [],
-  buttonsText:[],
+  buttonsText: [],
   addTag: function (tag, template) {
-    [...document.querySelectorAll(`${tag}`)].forEach((el,index) => {
+    [...document.querySelectorAll(`${tag}`)].forEach((el, index) => {
       let currentTemplate = template;
       const attrList = [...el.attributes];
 
-      this.buttonsText.push(el.innerText)
+      this.buttonsText.push(el.innerText);
 
       attrList.forEach(attr => {
-        if(currentTemplate.indexOf(attr.localName) === -1){
-          let addAttr = currentTemplate.split(' ')
-          addAttr[0]=`${addAttr[0]} ${attr.localName}=${attr.value}`
-          currentTemplate = addAttr.join(" ")
+        if (!currentTemplate.includes(attr.localName)) {
+          let addAttr = currentTemplate.split('>');
+
+          addAttr[0] = `${addAttr[0]} ${attr.localName}="${attr.value}"`;
+          currentTemplate = addAttr.join('>');
         }
-        currentTemplate = currentTemplate.replace(`{{${attr.localName}}}`, attr.value );
       });
-      currentTemplate = currentTemplate.replace(`{{html}}`, !this.buttonsText[index] ? 'Some Text'  : this.buttonsText[index] );
+      currentTemplate = currentTemplate.replace('{{html}}', this.buttonsText[index] || 'Some Text');
       this.tags.push({
         element: el,
         template: currentTemplate,
@@ -38,12 +38,21 @@ const Templater = {
 
 ;(function ($) {
   $.fn.templater = run;
+
   function run({ tags }) {
     let [currentTag] = this.children();
     let text = this.children().text().trim();
-    Object.values(currentTag.attributes).forEach(attr => {
-      tags.button = tags.button.replace(`{{${attr.localName}}}`, attr.value);
+
+    $(this.children()).each(function() {
+      $.each(this.attributes, function() {
+        if(this.specified) {
+          tags.button = tags.button.replace(`{{${this.name}}}`, this.value);
+        }
+      });
     });
-    this[0].innerHTML = tags.button.replace(/{{(.*?)}}/, text||'Some Text');
+    // Object.values(currentTag.attributes).forEach(attr => {
+    //   tags.button = tags.button.replace(`{{${attr.localName}}}`, attr.value);
+    // });
+    this.html(tags.button.replace(/{{(.*?)}}/, text || 'Some Text'))
   }
 })(jQuery);
